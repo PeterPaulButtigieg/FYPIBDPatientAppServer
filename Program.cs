10e832722f865c1591a3fc2273129ddbc51c2447
PeterPaulButtigieg <119859432+PeterPaulButtigieg@users.noreply.github.com>
2025-03-23 20:39:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddScoped<FYPIBDPatientApp.Services.TokenService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -95,6 +105,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 
